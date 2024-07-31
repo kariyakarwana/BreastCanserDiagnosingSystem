f111cbbd6733eb772594641abbd66dc0c1dff6b5
kariyakarwana <167743818+kariyakarwana@users.noreply.github.com>
2024-07-31 18:25:51 +0530

    Add files via upload

--- src/componants/NaviBox.js
+++ src/componants/NaviBox.js
@@ -1,40 +1,38 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Switch directly from react-router-dom
-import { Link } from 'react-router-dom';
-import './navi.css'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import './navi.css';
 import Home from '../Pages/Home';
-import Wheretogo from '../Pages/Wheretogo';
+import Awareness from '../Pages/Awareness';
 import Whatotdo from '../Pages/Whatotdo';
-import Aboutus from '../Pages/Aboutus';
+import Suwanari from '../Pages/Suwanari';
 import Signin from '../Pages/Sin';
 import Signup from '../Pages/Sup';
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600;700&display=swap"></link>
 
 export default function NaviBox() {
   return (
     <Router>
       <div>
         <div className="naviBox">
-          <div className="logo">
-            <img src="" alt="Logo" /> 
+        <div className="logo">
+            <img src="path/to/your/logo.png" alt="Logo" /> {/* Provide the correct path to your logo */}
           </div>
           <div className="tabs">
             <Link to="/">HOME</Link>
-            <Link to="/wheretogo">Where to go</Link>
-            <Link to="/whatotdo">What to do</Link>
-            <Link to="/aboutus">About Us</Link>
+            <Link to="/awareness">Awareness</Link>
+            <Link to="/whatotdo">Self Assessment</Link>
+            <Link to="/suwanari">Suwanari</Link>
             <Link to="/signin">Sign In</Link>
             <Link to="/signup">Sign Up</Link>
           </div>
+          
         </div>
-        <Routes> {/* Use Routes component */}
+        <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/wheretogo" element={<Wheretogo />} />
+          <Route path="/awareness" element={<Awareness />} />
           <Route path="/whatotdo" element={<Whatotdo />} />
-          <Route path="/aboutus" element={<Aboutus />} />
+          <Route path="/suwanari" element={<Suwanari />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
-          {/* Define routes for other tabs */}
         </Routes>
       </div>
     </Router>
